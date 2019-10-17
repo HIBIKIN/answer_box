115a0d669ca2439521c1228541f1c0eece352f88
HIBIKIN <hi3bi1ki5@gmail.com>
2019-10-17 21:35:11 +0900

    簡単なツイート昨日の実装

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,4 +1,5 @@
 class RepliesController < ApplicationController
+  before_action :set_twitter_client
   before_action :set_answer, only: [:new, :create]
   before_action :set_theme, only: [:new, :create]
 
@@ -13,6 +14,7 @@ class RepliesController < ApplicationController
       answer_id: params[:answer_id]
     }
     if @reply.save
+      @twitter.update("#{@reply.body}\nテストテスト")
       redirect_to "/themes/#{@answer.theme_id}/answers/#{@answer.id}", notice: "返答を送りました。"
     else
       render :new
@@ -31,5 +33,14 @@ class RepliesController < ApplicationController
 
     def set_theme
       @theme = Theme.find(@answer.theme_id)
+    end
+
+    def set_twitter_client
+      @twitter = Twitter::REST::Client.new do |config|
+        config.consumer_key        = "Y2c4X637cQBHfx2IJ6UWVxGtb"
+        config.consumer_secret     = "fAlNRrnPx2vJ7EAThpSQpBuj3BePpL5st3KgRYPKIwl2bEM86I"
+        config.access_token        = "994147378323013632-p8UxTBBKcsMt46E9nKBZ6HLiNjJ1I5G"
+        config.access_token_secret = "FAZn2xSRfLWUCo1C7MBvpFzJ50QD4Eis19VTCIHlmM0Fk"
+      end
     end
 end
--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,5 +1,7 @@
 class ThemesController < ApplicationController
   require 'uri'
+  before_action :set_twitter_client
+
 
   def home
   end
@@ -24,6 +26,7 @@ class ThemesController < ApplicationController
     # ここでツイートモスルかで条件分岐
     # redirect_to themes_url, notice: "書籍を登録しました。"
     if @theme.save
+      @twitter.update("#{@theme.title}\nテストテスト")
       redirect_to @theme, notice: "逆質問を作成しました。回答を募集しましょう！"
     else
       render :new
@@ -47,5 +50,15 @@ class ThemesController < ApplicationController
 
     def theme_params
       params.require(:theme).permit(:title, :description)
+    end
+
+
+    def set_twitter_client
+      @twitter = Twitter::REST::Client.new do |config|
+        config.consumer_key        = "Y2c4X637cQBHfx2IJ6UWVxGtb"
+        config.consumer_secret     = "fAlNRrnPx2vJ7EAThpSQpBuj3BePpL5st3KgRYPKIwl2bEM86I"
+        config.access_token        = "994147378323013632-p8UxTBBKcsMt46E9nKBZ6HLiNjJ1I5G"
+        config.access_token_secret = "FAZn2xSRfLWUCo1C7MBvpFzJ50QD4Eis19VTCIHlmM0Fk"
+      end
     end
 end
