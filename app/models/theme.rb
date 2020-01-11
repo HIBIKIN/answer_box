f34ddf9b95069965fa5ae56eca6c8f8d8935b3e5
HIBIKIN <hi3bi1ki5@gmail.com>
2020-01-11 21:14:30 +0900

    homeページのデザインをめっちゃ良くしました！！

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,6 +1,6 @@
 class Theme < ApplicationRecord
   has_many :answer, dependent: :destroy
   belongs_to :user
-  validates :title, presence: true, length: { maximum: 85 }
+  validates :title, presence: true, length: { maximum: 30 }
   validates :user_id, presence: true
 end
